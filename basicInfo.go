3eef3040661e24ba3c6f30aaebeb221f31f3cb06
mcshmarckledorf <joeygame@gmail.com>
2017-05-05 20:22:51 -0700

    Delete basicInfo.go

--- basicInfo.go
+++
@@ -1,5 +0,0 @@
-package stringUtil
-
-var MyName = " Joseph Lorant\n"
-var MyAddress = " 3041 W 7th St.\n Los Angeles, CA 90005\n"
-var MyEducation = " San Luis Obispo High School\n Cuesta Community Colelge\n UC Santa Cruz\n Southwestern University School of Law\n"
